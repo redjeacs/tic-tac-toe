574446cac724b44ac6325c8a16ca6ef38735f2b1
Jerry Chang <213577976+redjeacs@users.noreply.github.com>
2025-07-06 14:41:00 +0800

    GameController object
    store an array of two players with their tokens in
    variable players. activePlayer variable starts
    with player one. switch player internal funciton
    checks the activePlayer and switches it to the
    other player. getActivePlayer function retrieves
    activePlayer for use in ui later. playRound
    function is used to mark the chosen cell switch
    player and print out the current board situation.

--- script.js
+++ script.js
@@ -16,10 +16,10 @@ function Gameboard() {
   const getBoard = () => board; // a stored function to get the board for the ui
 
   const markCell = (row, column, player) => { 
-    if(board[row][column].getValue !== 0) { // takes the row and column of the cell and checks if it's already been marked
+    if(board[row - 1][column - 1].getValue !== 0) { // takes the row and column of the cell and checks if it's already been marked
       return;
     };
-    board[row][column].addToken(player); //mark the cell with the player token
+    return board[row - 1][column - 1].addToken(player); //mark the cell with the player token
   }
 
   const printBoard = () => { //only for console game (not needed after ui is built)
@@ -44,4 +44,31 @@ function Cell() {
 
 function GameController(playerOneName = 'Player One', playerTwoName = 'Player Two') { //object to control the flow of the game (factory function)
   const board = Gameboard();
+
+  const players = [{name: playerOneName, token: 1}, {name: playerTwoName, token: 2}]; //an array with two object for the two players
+
+  let activePlayer = players[0]; //first player is player one in index 0
+
+  const switchPlayerTurn = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0]; //if activeplayer is player one change to player 2 otherwise chagne to player one
+  };
+  const getActivePlayer = () => activePlayer; //retrieve current activePlayer object
+
+  const printNewRound = () => { //console log current board condition and new player's turn
+    board.printBoard();
+    console.log(`${getActivePlayer}'s turn`);
+  }
+
+  const playRound = (row, column) => {
+    console.log(`Dropping ${getActivePlayer}'s token into row ${row} and column ${column}`) //for console game
+    board.markCell(row, column, getActivePlayer().token); //mark the cell at row and column with activeplayer's token
+    switchPlayerTurn();
+    printNewRound();
+  };
+  
+  printNewRound(); //start of game message for fresh board
+
+  return { playRound, getActivePlayer } //object to play out rounds of tic tac toe with getActivePlayer object for ui
 }
+
+const game = GameController();
